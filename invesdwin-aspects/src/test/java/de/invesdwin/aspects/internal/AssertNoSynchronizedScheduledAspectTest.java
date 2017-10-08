ff3745a3aff57aaea9855222071f3551deffcb5d
subes <gsubes@gmail.com>
2017-10-08 23:16:38 +0200

    dependency updates and some java9 preparations

--- invesdwin-aspects/src/test/java/de/invesdwin/aspects/internal/AssertNoSynchronizedScheduledAspectTest.java
+++ invesdwin-aspects/src/test/java/de/invesdwin/aspects/internal/AssertNoSynchronizedScheduledAspectTest.java
@@ -3,8 +3,7 @@ package de.invesdwin.aspects.internal;
 import javax.annotation.concurrent.ThreadSafe;
 
 import org.assertj.core.api.Fail;
-import org.junit.ComparisonFailure;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.scheduling.annotation.Scheduled;
 
 import de.invesdwin.aspects.annotation.SkipParallelExecution;
@@ -22,10 +21,9 @@ public class AssertNoSynchronizedScheduledAspectTest {
         try {
             new ScheduledTestBean().scheduleSynchronized();
             Fail.fail("Exception expected");
-        } catch (final ComparisonFailure e) {
-            Assertions.assertThat(e.getMessage())
-                    .contains("@" + Scheduled.class.getSimpleName())
-                    .contains("@" + SkipParallelExecution.class.getSimpleName());
+        } catch (final Throwable e) {
+            Assertions.assertThat(e.getMessage()).contains("@" + Scheduled.class.getSimpleName()).contains(
+                    "@" + SkipParallelExecution.class.getSimpleName());
         }
     }
 
--- invesdwin-aspects/src/test/java/de/invesdwin/aspects/internal/MonitoredAspectTest.java
+++ invesdwin-aspects/src/test/java/de/invesdwin/aspects/internal/MonitoredAspectTest.java
@@ -4,7 +4,7 @@ import java.util.concurrent.TimeUnit;
 
 import javax.annotation.concurrent.ThreadSafe;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import com.jamonapi.MonitorFactory;
 
