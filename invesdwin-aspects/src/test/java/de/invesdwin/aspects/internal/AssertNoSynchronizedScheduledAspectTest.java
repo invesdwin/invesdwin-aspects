74888c50c37c349d84cd50fe6f420ebe27f239b1
subes <gsubes@gmail.com>
2019-06-21 15:29:07 +0100

    ignore until aspectj 1.9.5

--- invesdwin-aspects/src/test/java/de/invesdwin/aspects/internal/AssertNoSynchronizedScheduledAspectTest.java
+++ invesdwin-aspects/src/test/java/de/invesdwin/aspects/internal/AssertNoSynchronizedScheduledAspectTest.java
@@ -3,6 +3,7 @@ package de.invesdwin.aspects.internal;
 import javax.annotation.concurrent.ThreadSafe;
 
 import org.assertj.core.api.Fail;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.scheduling.annotation.Scheduled;
 
@@ -10,6 +11,7 @@ import de.invesdwin.aspects.InstrumentationTestInitializer;
 import de.invesdwin.aspects.annotation.SkipParallelExecution;
 import de.invesdwin.util.assertions.Assertions;
 
+@Ignore("will be fixed with aspectj 1.9.5")
 @ThreadSafe
 public class AssertNoSynchronizedScheduledAspectTest {
 
